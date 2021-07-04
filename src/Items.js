9a0adffc5f915c2659b5a0c99b8f0863d68c4ff0
Prince Patel <69680860+princepatel157@users.noreply.github.com>
2021-07-04 07:27:59 +0530

    full webpage

--- src/Items.js
+++ src/Items.js
@@ -1,45 +1,55 @@
 const Items = [
   {
     id: 1,
-    name:
-      "Download The Last Kingdom (Season 1-4) Dual Audio {Hindi-English} 480p [150MB] || 720p [350MB]",
+    name: "The Last Kingdom (Season 1-4) Dual Audio {Hindi-English} 480p [150MB] || 720p [350MB]",
     image: "tlk.jpg",
   },
   {
     id: 2,
-    name:
-      "Download Game of Thrones (Season 1-8) {English With Subtitles} 480p [200MB] || 720p [400MB]",
+    name: "Game of Thrones (Season 1-8) {English With Subtitles} 480p [200MB] || 720p [400MB]",
     image: "got.jpg",
   },
   {
     id: 3,
-    name:
-      "Download The Game (1997) {English With Subtitles} BluRay 480p [500MB] || 720p [1.0GB] || 1080p [2.0GB]",
-    image: "game.jpg",
+
+    name: "Loki (Season 1) [S01E04] Dual Audio {Hindi-English} 480p [150MB] || 720p [400MB] || 1080p [1.2GB]",
+    image: "loki.jpg",
   },
   {
     id: 4,
-    name:
-      "Download The Boys (Season 1-2) Dual Audio {Hindi-English} WeB-HD 480p [200MB] || 720p [350MB]",
+    name: "The Boys (Season 1-2) Dual Audio {Hindi-English} WeB-HD 480p [200MB] || 720p [350MB]",
     image: "boys.jpg",
   },
   {
     id: 5,
-    name:
-      "Download Marvel Studios: Legends (Season 1) {English With Subtitles} WeB-HD 720p [70MB] || 1080p [180MB]",
-    image: "legends.jpg",
+
+    name: "The Boss Baby: Family Business (2021) {English With Subtitles} WeB-DL 480p [350MB] || 720p [800MB] || 1080p [2.1GB]",
+    image: "boss.jpg",
   },
   {
     id: 6,
-    name:
-      "Download Marvel Studios: Legends (Season 1) {English With Subtitles} WeB-HD 720p [70MB] || 1080p [180MB]",
-    image: "legends.jpg",
+    name: "The Hitman’s Wife’s Bodyguard (2021) {English Audio} WeB-HD RiP 480p [350MB] || 720p [851MB] || 1080p [1.9GB]",
+    image: "hit.jpg",
   },
   {
     id: 7,
-    name:
-      "Download Marvel Studios: Legends (Season 1) {English With Subtitles} WeB-HD 720p [70MB] || 1080p [180MB]",
+    name: "The Tomorrow War (2021) Dual Audio {Hindi-English} WeB-DL HD 480p [490MB] || 720p [1.2GB] || 1080p [3.1GB]",
+    image: "war.jpg",
+  },
+  {
+    id: 8,
+    name: "Marvel Studios: Legends (Season 1) {English With Subtitles} WeB-HD 720p [70MB] || 1080p [180MB]",
     image: "legends.jpg",
+  },
+  {
+    id: 9,
+    name: "The Game (1997) {English With Subtitles} BluRay 480p [500MB] || 720p [1.0GB] || 1080p [2.0GB]",
+    image: "game.jpg",
+  },
+  {
+    id: 10,
+    name: "Spirit Untamed (2021) {English With Subtitles} BluRay 480p [250MB] || 720p [750MB]",
+    image: "sprint.jpg",
   },
 ];
 export default Items;
